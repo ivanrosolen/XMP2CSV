d84d9d73147d99d234526717c78dec54f45e1746
Ivan Rosolen <ivan@rosolen.net>
2015-04-13 10:40:19 -0400

    initial commit

---
+++ getXMP.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+touch csv_ninja.csv
+
+find . -name '*.psd' | while read file; do
+
+    xmp=$(exiftool -xmp -b "${file}" | egrep 'xwnv:(.+)=(.+)"$' | cut -d : -f 2- | tr "\n" ";")
+
+    filename=$(basename "${file}")
+
+    echo "${file};${filename};${xmp%?}" >> csv_ninja.csv
+
+    echo "Extracting: ${file}"
+
+done
